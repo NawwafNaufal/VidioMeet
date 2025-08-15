6c099bc6430119462edfc0fd6d25ef43de5163fb
NawwafNaufal <nawwafnaufal03@gmail.com>
2025-08-15 10:43:09 +0800

    Change getUsers

--- Backend/Controllers/Admin/getUsers.controller.js
+++ Backend/Controllers/Admin/getUsers.controller.js
@@ -2,7 +2,33 @@ const Users = require("../../Models/SignUpDB")
 const logger = require('../../log/Winston')
 
 const getUsersController =async (req,res) => {
-   
+    try {
+        const page = parseInt(req.query.page) || 1
+        const limit = parseInt(req.query.limit) || 10
+        const filterRole = req.query.role
+
+        const skip = (page - 1) * limit
+
+        const result = filterRole && {role : filterRole} || {}
+
+        const getUser =await Users.find(result)
+        .limit(limit)
+        .skip(skip)
+        .sort({username : -1})
+
+        const total = await Users.countDocuments(result)
+        
+        return res.status(200).json({
+            page,
+            limit,
+            role : filterRole || "All",
+            totalPage : Math.ceil(total / limit),
+            totaldata : total,
+            data : getUser
+        })
+    } catch (error) {
+        console.log(error)
+    }
     
 }
 
