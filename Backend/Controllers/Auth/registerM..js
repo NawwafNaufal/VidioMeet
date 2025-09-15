1d5e13da2ce2f6d2b152c9a306e62fcd808ea776
NawwafNaufal <nawwafnaufal03@gmail.com>
2025-09-15 09:38:54 +0700

    Fix signup bug

--- Backend/Controllers/Auth/registerM..js
+++ Backend/Controllers/Auth/registerM..js
@@ -2,12 +2,7 @@ const valiSignup = require('../../Services/ValidateSignUp')
 const logger = require('../../log/Winston')
 
 const signUpController = async (req,res,next) => {
-    console.log('=== DEBUG CONTROLLER ===')
-    console.log('req.body:', req.body)
-    console.log('req.headers:', req.headers)
-    console.log('========================')
-    const {username,email,password,dateOfBirth} = req.body
-    console.log(username,email,password,dateOfBirth)
+    const {username,email,password,dateOfBirth} = req.fullNameValidate
 
     try {      
         const {otpCode,otpResult} =await valiSignup(username,email,password,dateOfBirth)  
