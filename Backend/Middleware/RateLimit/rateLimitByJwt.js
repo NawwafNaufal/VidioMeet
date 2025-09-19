79491daf651c93cbeca52b459c31b0e07199e6f0
NawwafNaufal <nawwafnaufal03@gmail.com>
2025-09-19 10:16:38 +0700

    add rate limit by jwt

--- Backend/Middleware/RateLimit/rateLimitByJwt.js
+++ Backend/Middleware/RateLimit/rateLimitByJwt.js
@@ -1,14 +1,17 @@
 const {rateLimit} = require('express-rate-limit')
 
-const limitApi = rateLimit ({
-                windowMs : 1 * 60 * 1000,
-                max : 5,
+const limitByJwt = rateLimit ({
+                windowMs : 5 * 60 * 1000,
+                max : 3,
                 standardHeaders :true,
                 legacyHeaders : false,
+                keyGenerator : (req,res) => {
+                        return req.result?._id || req.ip
+                },
                 message : {
                         statut: 429,
                         message:"Request to many, please try again later"
                 } 
         }) 
 
-module.exports = limitApi
+module.exports = limitByJwt
--- Backend/Routes/Admin/getUsers.js
+++ Backend/Routes/Admin/getUsers.js
@@ -4,7 +4,8 @@ const routes = express.Router()
 const getUsersController = require('../../Controllers/Admin/getUsers')
 const validateJwt = require("../../Middleware/Auth/jwtValidate")
 const validateGetUsers  = require("../../Middleware/Validation/validateGetUsers")
+const limitByJwt = require("../../Middleware/RateLimit/rateLimitByJwt")
 
-routes.get('/get-users',validateJwt,validateGetUsers,getUsersController)
+routes.get('/get-users',validateJwt,limitByJwt,validateGetUsers,getUsersController)
 
 module.exports = routes
